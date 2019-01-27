0cf841a8b5b30de90b84938d83e0bcdfb72a8526
DGB Development <dgbdevelopment@gmail.com>
2019-01-27 19:27:34 +0100

    Segundo

--- mail-sender.js
+++ mail-sender.js
@@ -1,22 +1,21 @@
 const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
+
 const app = express();
 const email_config = require('./email-config');
 
 app.set('port', process.env.PORT || 3100)
 
-app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(cors());
 
 
 app.post('/send-email', (req, res) => {
-   console.log(req);
    const formulario = req.body;
-   console.log(formulario); 
    email_config(formulario);
-   res.status(200).send('Mensaje enviado correctamente');  
+   res.status(200).send();
 });
 
 
